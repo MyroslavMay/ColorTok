a73b920877cf78eff67e945e464dd4abea97bf45
MyroslavMay <myroslav.m2014@gmail.com>
2025-03-29 11:47:22 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ColorTok',
+  webDir: 'dist'
+};
+
+export default config;

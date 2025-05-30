1c4ad2b48f114e95f144fe7ee91412f14e15ae9b
Myroslav Maikovskiy <myroslav.m2014@gmail.com>
2025-05-30 17:45:08 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.MyroslavMay.ColorTok',
+  appName: 'ColorTok',
+  webDir: 'dist'
+};
+
+export default config;

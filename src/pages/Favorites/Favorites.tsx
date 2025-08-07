fceddc1c4512dc82bc87e37cc4bdb22febb3e234
MyroslavMay <myroslav.m2014@gmail.com>
2025-08-07 14:31:27 +0300

    Finally some commits

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,7 +1,9 @@
 const Favorites: React.FC = () => {
-    return (
-        <p>TODO: implement this</p>
-    )
-}
+  return (
+    <div style={{ backgroundColor: 'yellow', height: '100vh' }}>
+      <p>Favorites page</p>
+    </div>
+  );
+};
 
 export default Favorites;
